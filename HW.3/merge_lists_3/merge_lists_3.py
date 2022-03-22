6ff306f3042733c357e712a99db8e41eaa3c9d8c
Aleksandr Avdyushenko <ovalur@gmail.com>
2022-03-22 12:59:54 +0300

    Add HW.3

---
+++ HW.3/merge_lists_3/merge_lists_3.py
@@ -0,0 +1,10 @@
+import typing as tp
+
+
+def merge(input_streams: tp.Sequence[tp.IO[bytes]], output_stream: tp.IO[bytes]) -> None:
+    """
+    Merge input_streams in output_stream
+    :param input_streams: list of input streams. Contains byte-strings separated by "\n". Nonempty stream ends with "\n"
+    :param output_stream: output stream. Contains byte-strings separated by "\n". Nonempty stream ends with "\n"
+    :return: None
+    """
